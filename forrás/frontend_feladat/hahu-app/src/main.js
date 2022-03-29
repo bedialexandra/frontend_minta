4d6de0732ce8add8a68f52fb11890eaf43e79d04
bedialexandra <bedialexandra2003@gmail.com>
2022-03-29 11:33:15 +0200

    vue frontend

--- forrás/frontend_feladat/hahu-app/src/main.js
+++ forrás/frontend_feladat/hahu-app/src/main.js
@@ -2,6 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
-const app = createApp(App);
+import { createRouter, createWebHistory } from "vue-router";
 
+const app = createApp(App);
+const router = createRouter({
+  history: createWebHistory,
+  routes: [
+    { path: "/", component: OpenPage },
+    { path: "/offers", component: IngatlanKinalat },
+  ],
+});
+app.use(router);
 app.mount("#app");
